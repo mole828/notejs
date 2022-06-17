a1d4a0338966103f4f9d5416af83d630ee8cb6b5
mole828 <mrx800@163.com>
2022-06-17 14:38:58 +0800

    test

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ new Promise((resolve)=>{
 })
 for(const p of range(2))console.log(p)
 for(const p of primes(100))console.log(p)
+
+function argTest({func, ...params}){
+    console.log( func, params )
+    return func(params)
+}
+
+function f({x}){
+    return x**2
+}
+
+console.log(argTest({func:f, x:10}))
+
+class Person{
+    constructor({name='',age=0}){
+        this.name   = name;
+        this.age    = age;
+    }
+}
+let p = new Person({name:'bob'})
+console.log(p)
